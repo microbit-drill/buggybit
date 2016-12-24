a25cadca7667a8ebfca22344f9129912400b6f48
Owen Brotherwood <owen@brotherwood.biz>
2016-12-24 20:01:09 +0100

    face

--- linefollower/buggybit-linefollower.js
+++ linefollower/buggybit-linefollower.js
@@ -1,5 +1,9 @@
 let start = false
 basic.forever(() => {
+    pins.digitalWritePin(DigitalPin.P8, 0)
+    pins.digitalWritePin(DigitalPin.P12, 0)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
     basic.showLeds(`
         . . . . .
         . # . # .
@@ -8,17 +12,13 @@ basic.forever(() => {
         . # # # .
         `)
     if (start) {
-        pins.digitalWritePin(DigitalPin.P8, 0)
-        pins.digitalWritePin(DigitalPin.P12, 0)
-        pins.digitalWritePin(DigitalPin.P0, 0)
-        pins.digitalWritePin(DigitalPin.P16, 0)
         if (pins.digitalReadPin(DigitalPin.P1) == pins.digitalReadPin(DigitalPin.P2)) {
             basic.showLeds(`
+                . . . . .
+                . # . # .
                 . . # . .
+                # . . . #
                 . # # # .
-                # . # . #
-                . . # . .
-                . . # . .
                 `)
             pins.digitalWritePin(DigitalPin.P8, 0)
             pins.digitalWritePin(DigitalPin.P12, 1)
@@ -26,11 +26,11 @@ basic.forever(() => {
             pins.digitalWritePin(DigitalPin.P16, 1)
         } else if (pins.digitalReadPin(DigitalPin.P2) == 1) {
             basic.showLeds(`
-                # # # # .
-                # # . . .
-                # . # . .
-                # . . # .
-                . . . . #
+                # . . . .
+                . # . # .
+                . . # . .
+                # . . . #
+                . # # # .
                 `)
             pins.digitalWritePin(DigitalPin.P8, 0)
             pins.digitalWritePin(DigitalPin.P12, 1)
@@ -38,11 +38,11 @@ basic.forever(() => {
             pins.digitalWritePin(DigitalPin.P16, 0)
         } else {
             basic.showLeds(`
-                . # # # #
-                . . . # #
-                . . # . #
-                . # . . #
-                # . . . .
+                . . . . #
+                . # . # .
+                . . # . .
+                # . . . #
+                . # # # .
                 `)
             pins.digitalWritePin(DigitalPin.P8, 0)
             pins.digitalWritePin(DigitalPin.P12, 0)
@@ -58,4 +58,5 @@ input.onButtonPressed(Button.B, () => {
 input.onButtonPressed(Button.A, () => {
     start = true
 })
+start = false
 start = false
