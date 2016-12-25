2bef546a77e1e01146423e8688c4b97196d048f5
Owen Brotherwood <owen@brotherwood.biz>
2016-12-25 19:44:38 +0100

    Joshua personality

--- linefollower/buggybit-linefollower.js
+++ linefollower/buggybit-linefollower.js
@@ -1,35 +1,53 @@
-let right = 0
 let left = 0
 let start = false
+let right = 0
 basic.forever(() => {
     pins.digitalWritePin(DigitalPin.P8, 0)
     pins.digitalWritePin(DigitalPin.P12, left)
     pins.digitalWritePin(DigitalPin.P0, 0)
     pins.digitalWritePin(DigitalPin.P16, right)
-    basic.showLeds(`
-        . . . . .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `)
     if (start) {
         if (pins.digitalReadPin(DigitalPin.P1) == pins.digitalReadPin(DigitalPin.P2)) {
             left = 1
             right = 1
-            led.plot(2, 2)
+            basic.showLeds(`
+                # # . # #
+                . # . # .
+                . . . . .
+                # # # # #
+                . # # # .
+                `)
         } else if (pins.digitalReadPin(DigitalPin.P2) == 1) {
             left = 1
             right = 0
-            led.plot(0,0)
+            basic.showLeds(`
+                . # . . #
+                # # . # #
+                . . . . .
+                # # # # #
+                . # # # .
+                `)
         } else {
             left = 0
             right = 1
-            led.plot(4,0)
+            basic.showLeds(`
+                # . . # .
+                # # . # #
+                . . . . .
+                # # # # #
+                . # # # .
+                `)
         }
     } else {
         left = 0
         right = 0
+        basic.showLeds(`
+            . # . # .
+            # # . # #
+            . . . . .
+            . # # # .
+            # # # # #
+            `)
     }
 })
 input.onButtonPressed(Button.A, () => {
